b357e515ca3f61e8113a558bc98f34036f8c709c
reemhersh <reemhersh@gmail.com>
2019-10-16 23:56:21 +0300

    add week2-2 file

---
+++ python.py/week2-2.py
@@ -0,0 +1,12 @@
+print('Numbers from 1 to 10')
+
+x=4
+
+while True:
+    if int(input("guess the Number:"))== x :
+        print('Great! You did it!')
+        break
+    else :
+        while True :
+            print('guess the Number:')
+            continue
