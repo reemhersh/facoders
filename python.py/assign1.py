392596ebf413b00c7cfbc53052f84bf393cb555f
reemhersh <reemhersh@gmail.com>
2019-10-16 23:51:32 +0300

    add assign1

---
+++ python.py/assign1.py
@@ -0,0 +1,19 @@
+# سنقوم بتخزين المدخل بمتغير ، المدخل هو 88
+
+mark = 68
+
+if mark<50 :
+    result='failed'
+elif mark<75 :
+    result='Accepted'
+elif mark<85 :
+    result='Good'
+elif mark<90 :
+    result='Very good'
+else:
+    result='Excellent'# ابدا بكتابة الكود هنا
+
+
+
+# قم بطباعة النتيجة
+print(result )
